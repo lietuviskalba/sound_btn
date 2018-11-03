99c88bea6161eac042f578d65d9e346ad83a7f88
LietuvisKalba <mantasling@gmail.com>
2018-11-03 20:29:32 +0000

    Pre optimize UI

---
+++ Assets/PlaySound.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class PlaySound : MonoBehaviour {
+
+    public GameObject soundBoard;
+    SelectSoundByte selectSound;
+    public Animator anim;
+    public LayerMask buttonMask;
+
+    void Start()
+    {
+        selectSound = soundBoard.GetComponent<SelectSoundByte>();
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetMouseButtonDown(0) && !EventSystem.current.IsPointerOverGameObject())
+        {
+            RaycastHit hit;
+            Ray toMouse = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(toMouse, out hit, buttonMask))
+            {               
+                anim.SetTrigger("press"); // Play the animation
+                selectSound.GetSelectedSound().Play(); // play the sound            
+            }            
+        }  
+    }
+
+    
+}
